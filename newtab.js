2279ae16a1ca8153ec2c3bb03ff7785ce8dbeeef
code-sharad <codesharad@gmail.com>
2024-05-19 20:26:44 +0530

    chore: Update newtab.html and newtab.js

--- newtab.js
+++ newtab.js
@@ -6,7 +6,6 @@ chrome.storage.local.get(["countdownStart", "countdownDuration"], (data) => {
     const elapsed = Math.floor((Date.now() - data.countdownStart) / 1000);
     const remainingSeconds = data.countdownDuration - elapsed;
     startCountdown(remainingSeconds);
-    
   }
 });
 
@@ -20,7 +19,7 @@ const minutesContainer = document.querySelector("#minutes");
 const secondsContainer = document.querySelector("#seconds");
 const countdownNum = document.getElementById("countdown");
 const countDownName = document.getElementById("name");
-
+const microsecondsContainer = document.querySelector("#micro"); 
 countDownName.style.display = "none";
 countdownNum.style.display = "none";
 
@@ -35,8 +34,8 @@ function startCountdown(remainingSeconds) {
       countdownContainer.textContent =
         "Time is up! Click on the Life Countdown extension icon to set timer";
       return;
-    }else{
-        countdownContainer.textContent = "";
+    } else {
+      countdownContainer.textContent = "";
     }
 
     const years = Math.floor(remainingSeconds / (365 * 24 * 3600));
@@ -45,17 +44,18 @@ function startCountdown(remainingSeconds) {
     );
     const hours = Math.floor((remainingSeconds % (24 * 3600)) / 3600);
     const minutes = Math.floor((remainingSeconds % 3600) / 60);
-    const seconds = remainingSeconds % 60;
-
+    const seconds = Math.floor(remainingSeconds % 60);
+    const microseconds = Math.floor((remainingSeconds % 1) * 1000);
     // countdownContainer.textContent = `${years}y ${days}d ${hours}h ${minutes}m ${seconds}s `;
     yearsContainer.textContent = years;
     daysContainer.textContent = days;
     hoursContainer.textContent = hours;
     minutesContainer.textContent = minutes;
     secondsContainer.textContent = seconds;
+    microsecondsContainer.textContent = microseconds;
 
-    remainingSeconds--;
-    setTimeout(updateTimer, 1000);
+    remainingSeconds -= 0.01;
+    setTimeout(updateTimer, 1);
   }
 
   updateTimer();
